d5e7fccf5d8f45853b5979324da7499825d81f28
Tom Conchie <tconchie@gmail.com>
2017-07-12 12:06:17 -0600

    Adds component for rewards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RewardsCarousel from './components/rewards.component.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,7 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <RewardsCarousel />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
